5800937b8d8d2d6ca4b0b928c1f6eabfe41f51cb
Perris2.0 <m.bravom@alumnos.duoc.cl>
2018-11-05 01:35:02 -0300

    Se agregaron los templates

--- apps/adopcion/admin.py
+++ apps/adopcion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.adopcion.models import Persona, Ciudad, Region, Vivienda
+
 # Register your models here.
+
+admin.site.register(Persona)
+admin.site.register(Ciudad)
+admin.site.register(Region)
+admin.site.register(Vivienda)
--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from apps.mascota.models import Mascota
 
 # Create your models here.
+
+class Region(models.Model):
+	nombre_region = models.CharField(max_length=50)
+
+class Ciudad(models.Model):
+	nombre_ciudad = models.CharField(max_length=50)
+
+class Vivienda(models.Model):
+	tipo_vivienda = models.CharField(max_length=50)
+
 
 class Persona(models.Model):
 	email = models.EmailField()
@@ -8,7 +19,7 @@ class Persona(models.Model):
 	nombre = models.CharField(max_length=50)
 	fecha_nacimiento = models.DateField()
 	telefono = models.CharField(max_length=12)
-	region = models.TextField()
-	ciudad = models.TextField()
-	tipo_vivienda = models.TextField()
+	Region = models.ManyToManyField(Region)
+	Ciudad = models.ManyToManyField(Ciudad)
+	Vivienda = models.ManyToManyField(Vivienda)
 
